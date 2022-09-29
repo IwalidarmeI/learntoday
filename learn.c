54700cee89eea38141d57374b461022fc50a4c4d
walid boughrara <wd.boughrara@gmail.com>
2022-09-29 20:52:25 +0000

    I learnt the variables and pointers

--- learn.c
+++ learn.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 
 
 int main()
 {
+	int dayNumber = 1;
+	char whatIlearn [] = "pointers and variables";  
  for (int i=0;i<3;i++)	{
-  printf("Hello World\n");
- }
+	 printf("the day number %d\n" ,dayNumber);
+  printf("Hello for today I learned %s\n",whatIlearn);
+ }//this for loop to print 3 statments//
   return 0;
 }
   
